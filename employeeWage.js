7d4dacedac50eaeeea7c8ebfc38e95f10e55fcb6
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-26 10:54:08 +0530

    ADDED:code to calculate daily employee wage

--- employeeWage.js
+++ employeeWage.js
@@ -1,7 +1,13 @@
-let isPresent=1;
+let EMP_RATE_PER_HOUR=20;
+let empHrs=0;
+let empWage=0;
+
 let empCheck=Math.floor(Math.random()*10)%2;
 if(empCheck==1)
-	console.log("Employee is Present");
+	empHrs=8;
 else
-	console.log("Employee is Absent");
+	empHrs=0;
 
+empWage=empHrs*EMP_RATE_PER_HOUR;
+
+console.log("one day employee wage :",empWage);
