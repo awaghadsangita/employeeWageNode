c9ad3f934dedbda14ac89b21024f84de0366f40f
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-26 15:34:21 +0530

    ADDED:part time employee wage

--- employeeWage.js
+++ employeeWage.js
@@ -1,13 +1,18 @@
+let IS_PART_TIME=1;
+let IS_FULL_TIME=2
 let EMP_RATE_PER_HOUR=20;
+
 let empHrs=0;
 let empWage=0;
 
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==1)
+let empCheck=Math.floor(Math.random()*10)%3;
+if(empCheck==IS_PART_TIME)
+	empHrs=4;
+else if(empCheck==IS_FULL_TIME)
 	empHrs=8;
 else
 	empHrs=0;
 
 empWage=empHrs*EMP_RATE_PER_HOUR;
 
-console.log("one day employee wage :",empWage);
+
