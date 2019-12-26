7cb722f1c2ada0a6aa16dfe4643a560b8484b533
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-26 17:03:52 +0530

    REFACTORED:replace if else logic with switch case

--- employeeWage.js
+++ employeeWage.js
@@ -6,12 +6,18 @@ let empHrs=0;
 let empWage=0;
 
 let empCheck=Math.floor(Math.random()*10)%3;
-if(empCheck==IS_PART_TIME)
-	empHrs=4;
-else if(empCheck==IS_FULL_TIME)
-	empHrs=8;
-else
-	empHrs=0;
+switch(empCheck){
+	case IS_PART_TIME:{
+		empHrs=4;
+		break;
+	}
+	case IS_FULL_TIME:{
+		empHrs=8;
+		break;
+	}
+	default:
+		empHrs=0;
+}
 
 empWage=empHrs*EMP_RATE_PER_HOUR;
 
