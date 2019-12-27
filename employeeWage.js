f654affd3dcc9bea442230b1345a6fed6dbe72da
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-27 09:50:15 +0530

    REFACTORED:Calculate Wages till a condition of total working hours of 100 or max days os 20 is reached

--- employeeWage.js
+++ employeeWage.js
@@ -1,13 +1,16 @@
 let IS_PART_TIME=1;
-let IS_FULL_TIME=2
+let IS_FULL_TIME=2;
+let MAX_HRS_IN_MONTH=100;
 let EMP_RATE_PER_HOUR=20;
-let NUM_OF_WORKING_DAYS=30;
+let NUM_OF_WORKING_DAYS=20;
 
 let empHrs=0;
-let empWage=0;
+let totalEmpHrs=0;
 let totalEmpWage=0;
+let totalWorkingDays=0;
 
-for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<=NUM_OF_WORKING_DAYS){
+	totalWorkingDays++;	
 	let empCheck=Math.floor(Math.random()*10)%3;
 	switch(empCheck){
 		case IS_PART_TIME:{
@@ -21,7 +24,6 @@ for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
 		default:
 			empHrs=0;
 	}
-empWage=empHrs*EMP_RATE_PER_HOUR;
-totalEmpWage+=empWage;
+	totalEmpHrs+=empHrs;
 }
-
+totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
